ad69f52db7e1a796fe7646cb5e3567b212912360
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2025-01-12 21:49:17 +0530

    add responsive header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = ({ title }) => {
+  return (
+    <header className="bg-[#f4f2e9] w-full flex items-center justify-between px-4 md:px-12 h-12 py-8">
+      <h1 className="text-2xl md:text-4xl">{title}</h1>
+
+      <button className="bg-[#f4f2e9] border-2 border-[#cfc9af] rounded-lg px-2 py-0.5 w-[5rem] md:w-[8rem] font-medium">
+        Login
+      </button>
+    </header>
+  );
+};
+
+export default Header;
